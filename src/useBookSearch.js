7eabb6cdf1bd404352bc71d327eba5fd14da5beb
Kato <kubkate@gmail.com>
2020-09-01 15:09:41 -0700

    add cancel method in axios to make only single request

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -8,16 +8,24 @@ import axios from 'axios';
  */
 export default function useBookSearch(query, pageNumber) {
 
+
   // call API everytime that query or pageNumber has changed
   // this API comes with builtin page
   useEffect(() => {
+    // store and return cancel method to prevent from ongoing fecthing data, so it will be only one request is made - not every type stroke
+    let cancel;
     axios({
       method: 'GET',
       url: 'http://openlibrary.org/search.json',
-      params: { q: query, page: pageNumber }
+      params: { q: query, page: pageNumber },
+      cancelToken: new axios.CancelToken(c => cancel = c)
     }).then(res => {
       console.log('response:', res.data);
+    }).catch(e => {
+      // use builtin axios method to ignore error id is happen from cancel method
+      if (axios.isCancel(e)) return
     })
+    return () => cancel()
   }, [query, pageNumber])
 
   return null
