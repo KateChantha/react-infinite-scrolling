f912348ea0dc536ce8cb56737b49ed896163a291
Kato <kubkate@gmail.com>
2020-09-01 13:48:55 -0700

    setup boilerplate in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    < >
+      <input type='text'></input>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Loading...</div>
+      <div>Error</div>
+    </>
+  )
 }
-
-export default App;
