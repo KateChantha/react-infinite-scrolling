e930568ecce6fb915787611604112c9b352b9823
Kato <kubkate@gmail.com>
2020-09-01 15:47:44 -0700

    reander books, loading, error in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import useBookSearch from './useBookSearch';
 export default function App() {
   const [query, setQuery] = useState('');
   const [pageNumber, setPageNumber] = useState(1);
-  useBookSearch(query, pageNumber)
+
+  // ===========================
+  // destructuring state that return from useBookSearch
+  // ===========================
+  const { books, error, loading, hasMore } = useBookSearch(query, pageNumber)
+
+
   const handleSearch = (e) => {
     setQuery(e.target.value);
     // everytime new query, set to start search at page 1 
@@ -15,13 +21,11 @@ export default function App() {
   return (
     < >
       <input type='text' onChange={handleSearch}></input>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Loading...</div>
-      <div>Error</div>
+      {books.map(book => {
+        return <div key={book}>{book}</div>
+      })}
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
     </>
   )
 }
--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -8,10 +8,19 @@ import axios from 'axios';
  */
 export default function useBookSearch(query, pageNumber) {
 
+  // initailize loading to true, because it is the first thing we do is load in side out app
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
 
   // call API everytime that query or pageNumber has changed
   // this API comes with builtin page
   useEffect(() => {
+    // ==== set state when a request is made ===
+    setLoading(true);
+    setError(false);
+
     // store and return cancel method to prevent from ongoing fecthing data, so it will be only one request is made - not every type stroke
     let cancel;
     axios({
@@ -20,13 +29,29 @@ export default function useBookSearch(query, pageNumber) {
       params: { q: query, page: pageNumber },
       cancelToken: new axios.CancelToken(c => cancel = c)
     }).then(res => {
+
+      // add more books result to provious state
+      // use new Set to return just unique title - no duplication and use spread operator to convert it back to an array
+      setBooks(prevBooks => {
+        return [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])];
+      })
+
+      // check if there is no more books to be load, if greater than 0 means we have no more data, so we knows that we don't need to make this query again
+      setHasMore(res.data.docs.length > 0)
+      // at the end of fething data, reset loading
+      setLoading(false);
+
       console.log('response:', res.data);
     }).catch(e => {
       // use builtin axios method to ignore error id is happen from cancel method
-      if (axios.isCancel(e)) return
+      if (axios.isCancel(e)) return;
+      setError(true);
     })
     return () => cancel()
   }, [query, pageNumber])
 
-  return null
+  // =====================================
+  //  return state from this custom hooks
+  // =====================================
+  return { loading, error, books, hasMore }
 }
